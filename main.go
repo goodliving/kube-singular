51b072878aca4f4b3feb72dac8f3022cf1d2d36b
unknown <panzm@trump.local>
2019-06-19 14:29:15 +0800

    init

--- main.go
+++ main.go
@@ -1,9 +1,18 @@
 package main
 
 import (
+	// "fmt"
 	"fyne.io/fyne/widget"
 	"fyne.io/fyne/app"
+	// "github.com/satori/go.uuid"
 )
+
+
+func create_uuid() {
+	// u1 := uuid.Must(uuid.NewV4())
+	// fmt.Printf("UUIDv4: %s\n", u1)
+	return
+}
 
 func main() {
 	app := app.New()
@@ -11,6 +20,7 @@ func main() {
 	w := app.NewWindow("Hello")
 	w.SetContent(widget.NewVBox(
 		widget.NewLabel("Hello Fyne!"),
+		widget.NewButton("uuid", create_uuid),
 		widget.NewButton("Quit", func() {
 			app.Quit()
 		}),
